ced1f36c9c2093a04f64734a9fae4ad1a70d66be
TrendCatcher <karleo@kyonggi.ac.kr>
2025-11-10 10:24:14 +0900

    Initial commit

---
+++ src/main/java/com/mindhub/platform_server/MindfulnessPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.platform_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindfulnessPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindfulnessPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindhub/platform_server/MindfulnessPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.platform_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindfulnessPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
